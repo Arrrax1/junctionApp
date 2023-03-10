c691192f10e111b09ce334da161d82a13f72248c
jaqee-a <bouderbalamohammed2000@gmail.com>
2023-03-11 00:42:59 +0100

    add login functionality

---
+++ scripts/requests.js
@@ -0,0 +1,63 @@
+const URL = "https://cf9e-105-109-192-130.eu.ngrok.io/api"
+
+const getTokenHeader = async () => {
+    try {
+        const token = await AsyncStorage.getItem("token")
+
+        return {"Authorization": token }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function tokenPostRequest(endPoint, body) {
+    try {
+        const header = await getTokenHeader()
+        return postRequest(endPoint, body, header)
+
+    } catch (err) {
+
+    }
+}
+
+async function tokenGetRequest(endPoint) {
+    try {
+        const header = await getTokenHeader()
+        console.log(header);
+        return getRequest(endPoint, header)
+
+    } catch (err) {
+
+    }
+}
+
+async function getRequest(endPoint, headers = {}) {
+    try {
+        const res = await fetch(URL + endPoint, {
+            method: "GET",
+            headers: {
+                ...headers,
+                "accept": "application/json",
+            }
+        })
+        return res.json()
+    } catch (err) {
+
+    }
+}
+async function postRequest(endPoint, body, headers = {}) {
+    try {
+        const res = await fetch(URL + endPoint, {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                ...headers,
+                "content-type": "application/json",
+                "accept": "application/json",
+            }
+        })
+        return res.json()
+    } catch (err) {
+
+    }
+}
