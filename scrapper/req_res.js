71a38c2a220e5915f30d611481ade05323b83324
jaqee-a <bouderbalamohammed2000@gmail.com>
2023-03-11 17:27:01 +0100

    add replies

--- scrapper/req_res.js
+++ scrapper/req_res.js
@@ -5,11 +5,28 @@ function copy_text() {
 
 document.getElementById('copy').addEventListener('click', copy_text);
 
-function call_api() {
+async function call_api() {
+    const email = document.querySelector('#ihtml').innerText;
+    const emailHash = window.btoa(encodeURIComponent(email));
     let query = document.getElementById('send_query').parentElement.parentElement.children[0].value;
-    console.log(query);
+
+    const session_id = localStorage.getItem(emailHash);
+    const response = await tokenPostRequest('/assist/append', {
+        message: query,
+        session_id: session_id
+    });
+
+    if(response.success)
+    {
+        const messages = JSON.parse(localStorage.getItem(session_id));
+        const content = response.data.message;
+
+        messages.push(content);
+        localStorage.setItem(session_id, JSON.stringify(messages));
+
+        document.getElementById('copy').parentElement.parentElement.children[0].innerText=content;
+    }
     //Fun
-    document.getElementById('copy').parentElement.parentElement.children[0].innerText="MESSAGE FROM API";
 }
 
 document.getElementById('send_query').addEventListener('click', call_api);
